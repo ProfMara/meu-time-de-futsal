7a7cce48dab75192ecc30de3220329fcf12da858
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-10-31 13:24:40 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,35 @@
+
+//criar a matriz
+
+function enviar()
+{
+    //guardar os nomes nas variáveis
+
+
+    //adicioná-los na matriz
+
+
+    //exibir os nomes
+
+
+    document.getElementById("enviarBotao").style.display = "none";
+    document.getElementById("organizarBotao").style.display = "inline-block";
+    document.getElementById("mostrarNomes").style.display = "block";
+  
+
+}
+
+function organizar(){
+    //esconder a lista
+    document.getElementById("mostrarNomes").style.display = "none";
+    document.getElementById("mostrarNomesOrganizado").style.display = "block";
+
+    //organizar a matriz
+
+    //exibir a matriz
+
+
+    
+
+}
+
